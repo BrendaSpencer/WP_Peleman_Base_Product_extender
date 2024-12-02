26256d88a1f8042c40ea9ed87f2439cf7bf22364
BrendaSpencer <spencerbernadette1@gmail.com>
2024-12-02 12:36:57 +0100

    add styles for productpage

--- includes/Enqueue_Styles.php
+++ includes/Enqueue_Styles.php
@@ -7,13 +7,27 @@ namespace WSPBPE\includes;
 class Enqueue_Styles {
     public function __construct(){
         add_action('admin_enqueue_scripts', [$this,'enqueue_styles']);
+		add_action('wp_enqueue_scripts', [$this,'enqueue_productpage_styles']);
     }
 	
 	public function enqueue_styles(){
+
 		$url = WSPBPE_DIRECTORY . 'assets/css/style.css';
 		
         wp_enqueue_style(
             'admin_stylesheet',
+            $url,
+            array(),
+            '2.0.0',
+            'all'
+        );
+    }
+		public function enqueue_productpage_styles(){
+	
+		$url = WSPBPE_DIRECTORY . 'assets/css/productpage_style.css';
+		
+        wp_enqueue_style(
+            'productpage_stylesheet',
             $url,
             array(),
             '2.0.0',
